498a5ef6d0398f0c9b1b2072648c687fee8f00dc
Sultan Evgenii <esultan@saber.games>
2026-01-14 18:06:07 +0300

    add img, fonts, comp, scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import PizzaBlock from "./components/PizzaBlock";
+import Sort from "./components/Sort";
+import "./scss/app.scss";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>React pizza</h1>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock
+                title="Мексиканская"
+                price={395}
+                imageUrl="./img/pizza-hren.avif"
+                sizes={[26, 30, 40]}
+                types={["тонкое", "традиционное"]}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
